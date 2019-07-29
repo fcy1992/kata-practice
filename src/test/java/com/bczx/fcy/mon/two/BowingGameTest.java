7261d44f9dddab0d8b9fae986fda77652239857f
root <admin@example.com>
2019-07-30 00:50:10 +0800

    my second kata practice

---
+++ src/test/java/com/bczx/fcy/mon/two/BowingGameTest.java
@@ -0,0 +1,69 @@
+package com.bczx.fcy.mon.two;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BowingGameTest {
+
+  private BowingGame g;
+
+  @Before
+  public void setup () {
+     g = new BowingGame();
+  }
+
+  @Test
+  public void testGetGame() {
+    int n = 20;
+    int pins = 0;
+    rollMany(n, pins);
+    assertEquals(0, g.score());
+  }
+
+  private void rollMany(int n, int pins) {
+    for (int i = 0; i < n; i++) {
+      g.roll(pins);
+    }
+  }
+
+  @Test
+  public void testALlOnes() {
+    rollMany(20, 1);
+    assertEquals(20, g.score());
+  }
+
+  @Test
+  public void testOneSpare() {
+    rollSpare();
+    g.roll(3);
+    rollMany(17, 0);
+    assertEquals(16, g.score());
+  }
+
+  private void rollSpare() {
+    g.roll(5);
+    g.roll(5);
+  }
+
+  @Test
+  public void testOneStrike() {
+    rollStrike();
+    g.roll(3);
+    g.roll(4);
+    rollMany(16, 0);
+    assertEquals(24, g.score());
+  }
+
+  void rollStrike() {
+    g.roll(10);
+  }
+
+  @Test
+  public void testPerfectGame() {
+    rollMany(12,10);
+    assertEquals(300, g.score());
+  }
+
+}
