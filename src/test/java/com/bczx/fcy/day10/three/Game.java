23d511694f68fdc404b564a100eb23411f130f9d
root <admin@example.com>
2019-08-08 00:50:06 +0800

    my 10th kata practice

---
+++ src/test/java/com/bczx/fcy/day10/three/Game.java
@@ -0,0 +1,50 @@
+package com.bczx.fcy.day10.three;
+
+public class Game {
+
+  private int[] rolls = new int[21];
+
+  private int currentRoll;
+
+  public void roll(int pins) {
+    rolls[currentRoll ++] = pins;
+  }
+
+  public int score() {
+    int score = 0;
+    int rollIndex = 0;
+    for (int i = 0; i < 10; i++) {
+      if (isaSpare(rollIndex)) {
+        score += spareBonus(rollIndex);
+        rollIndex += 2;
+      } else if (isaStrike(rollIndex)) {
+        score += strikeBonus(rollIndex);
+        rollIndex += 1;
+      } else {
+        score += normalScore(rollIndex);
+        rollIndex += 2;
+      }
+    }
+    return score;
+  }
+
+  private int normalScore(int rollIndex) {
+    return rolls[rollIndex] + rolls[rollIndex + 1];
+  }
+
+  private int strikeBonus(int rollIndex) {
+    return 10 + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+  }
+
+  private boolean isaStrike(int rollIndex) {
+    return rolls[rollIndex] == 10;
+  }
+
+  private int spareBonus(int rollIndex) {
+    return 10 + rolls[rollIndex + 2];
+  }
+
+  private boolean isaSpare(int rollIndex) {
+    return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
+  }
+}
